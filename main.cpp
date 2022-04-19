f2f7eb8bbd403ee33c58b8ecb9ebbbb27c22fd2a
Timerbulat <TimerbulatGilmutdinov@github.com>
2022-04-19 15:40:33 +0300

    final commit with tests

--- main.cpp
+++ main.cpp
@@ -17,14 +17,42 @@ void testCaseFind(int k) {
     }
 }
 
-int main() {
+void testCaseRemove(int k){
+    for (int i = 0; i < k; i++) {
+        tree.remove(rand()%1000000);
+    }
+}
 
-    clock_t start = clock();
-    testCaseInsert(5000000);
-    cout << fixed << (double)(clock() - start) / CLOCKS_PER_SEC << '\n';
-    start = clock();
-    testCaseFind(10000000);
-    cout << fixed << (double)(clock() - start) / CLOCKS_PER_SEC << '\n';
+int main() {
+    int n = 5000000; //число повторений операции
+
+    Treap tree;
+    for (int j = 0; j < 100; ++j) {
+
+
+        auto start_time = std::chrono::steady_clock::now();
+        for (int i = 0; i < n; i++) {
+            tree.insert(rand());
+        }
+        auto end_time = std::chrono::steady_clock::now();
+        auto elapsed_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
+        std::cout << elapsed_ns.count() /n<< "\n"; //вывод времени в наносекундах на 1 операцию insert
+        start_time = std::chrono::steady_clock::now();
+        for (int i = 0; i < n; i++) {
+            tree.find(rand());
+        }
+        end_time = std::chrono::steady_clock::now();
+        elapsed_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
+        std::cout << elapsed_ns.count() /n<< "\n"; // вывод времени в наносекундах на 1 операцию find
+        start_time = std::chrono::steady_clock::now();
+        for (int i = 0; i < n; i++) {
+            tree.remove(rand());
+        }
+        end_time = std::chrono::steady_clock::now();
+        elapsed_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
+        std::cout << elapsed_ns.count() /n<< " \n"; //вывод времени в наносекундах на 1 операцию remove
+        tree.clear();
+    }
 
     return 0;
 }
